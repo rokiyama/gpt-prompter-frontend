38b7a495c732401b80185aee184f5934f3a8a108
rokiyama <57606749+rokiyama@users.noreply.github.com>
2023-04-21 17:01:04 +0900

    Refactoring: include asyncStorage to slice

--- src/redux/slices/settingsSlice.ts
+++ src/redux/slices/settingsSlice.ts
@@ -1,21 +1,40 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { loadSettings, saveSettings } from '../../utils/settingsPersistent';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { z } from 'zod';
 import { RootState } from '../store';
+import { uuid } from '../../utils/uuid';
 
-export interface SettingsState {
+const ASYNC_STORAGE_KEY_SETTINGS = 'SETTINGS';
+
+interface SettingsState {
   userId: string;
   apiKey: string;
   mode: 'userId' | 'apiKey';
 }
 
-export const initialState: SettingsState = {
+const initialState: SettingsState = {
   userId: '',
   apiKey: '',
   mode: 'userId',
 };
 
 export const load = createAsyncThunk('settings/load', async () => {
-  return await loadSettings();
+  const value = await AsyncStorage.getItem(ASYNC_STORAGE_KEY_SETTINGS);
+  try {
+    const obj = Settings.parse(JSON.parse(value ?? ''));
+    console.log(`load from asyncStorage`, obj);
+    return obj;
+  } catch (err) {
+    // ignore error
+    console.log('could not load settings', err);
+  }
+  // create new settings and save
+  const settings = {
+    ...initialState,
+    userId: uuid(),
+  };
+  await saveSettings(settings);
+  return settings;
 });
 
 export const save = createAsyncThunk(
@@ -30,9 +49,9 @@ export const settingsSlice = createSlice({
   name: 'settings',
   initialState,
   reducers: {
-    setApiKey: (state, action: PayloadAction<string>) => {
-      state.apiKey = action.payload;
-    },
+    //   setApiKey: (state, action: PayloadAction<string>) => {
+    //     state.apiKey = action.payload;
+    //   },
   },
   extraReducers: (builder) => {
     builder.addCase(load.fulfilled, (_, action) => {
@@ -51,3 +70,25 @@ export const selectApiKey = (state: RootState) => state.settings.apiKey;
 export const selectUseApiKey = (state: RootState) => state.settings.mode;
 
 // export default settingsSlice.reducer;
+
+const saveSettings = async (settings: SettingsState) => {
+  await AsyncStorage.setItem(
+    ASYNC_STORAGE_KEY_SETTINGS,
+    JSON.stringify(settings)
+  );
+};
+
+const schemaForType =
+  <T>() =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  <S extends z.ZodType<T, any, any>>(arg: S) => {
+    return arg;
+  };
+
+const Settings = schemaForType<SettingsState>()(
+  z.object({
+    userId: z.string(),
+    apiKey: z.string(),
+    mode: z.union([z.literal('userId'), z.literal('apiKey')]),
+  })
+);
