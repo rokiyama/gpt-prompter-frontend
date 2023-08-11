5c3acd9d439b5a5872fd101db00afdaf312569a9
rokiyama <57606749+rokiyama@users.noreply.github.com>
2023-08-11 17:57:58 +0900

    Bump version to v1.0.4

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'AI Prompt',
   slug: 'expo-chat-command-gpt',
-  version: '1.0.3',
+  version: '1.0.4',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'light',
